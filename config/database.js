132898c0ebbd4d13fb8997a43a39f29d28abedb1
Shraddha Kushwaha <shraddha123@gmail.com>
2025-12-19 21:58:19 +0530

    changes the database

--- config/database.js
+++ config/database.js
@@ -2,10 +2,17 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    const mongoURI = process.env.MONGO_URI;
+
+    if (!mongoURI) {
+      throw new Error("MONGO_URI is not defined in environment variables");
+    }
+
+    await mongoose.connect(mongoURI, {
       tls: true,
       tlsAllowInvalidCertificates: true
     });
+
     console.log("MongoDB Atlas Connected");
   } catch (error) {
     console.error("MongoDB Error:", error.message);
